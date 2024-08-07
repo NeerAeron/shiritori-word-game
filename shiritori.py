6f2a54ca6e4879275abd0f3b7915b7ac5ae400a2
Neer02 <neehir.aeron@gmail.com>
2024-08-06 23:53:23 -0700

    added contintents dictionary

--- shiritori.py
+++ shiritori.py
@@ -64,7 +64,8 @@ class Shirotori:
         },
         'atlas': {
             'dictionary': ['dictionaries/uscities.txt', 'dictionaries/usstates.txt', 
-                           'dictionaries/countries.txt', 'dictionaries/worldcities.txt'],
+                           'dictionaries/countries.txt', 'dictionaries/worldcities.txt',
+                           'continents.txt'],
             'countdown_time': 30,
             'input_type': 2
         },
